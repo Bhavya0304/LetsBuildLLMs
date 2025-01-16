e6c8ad033feb8ed32c77c57d8c6aa34a0a3f1743
Joshi Bhavya <jshi.bhavya@gmail.com>
2025-01-17 02:16:16 +0530

    init

---
+++ modules/neural_network/nn.py
@@ -0,0 +1,17 @@
+import torch as nn
+import numpy as np
+import pandas as pd
+
+
+# 4 x 100, [(11,)]
+
+class nn:
+    def __init__(self,input,hidden_layers,output):
+        pass
+    
+    def initNetwork(self):
+        pass
+    def Forward(self):
+        pass
+    def Backward(self):
+        pass
